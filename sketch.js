6f8dd7a61b080c24a8fc13f26e16e7729021af52
Robin Goyal <goyal.rob@gmail.com>
2017-05-02 16:20:17 -0400

    Ball bouncing between canvas

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 400;
+
+var circle = {
+    x: CANVAS_WIDTH/2,
+    y: CANVAS_HEIGHT/2,
+    diameter: 30,
+    xSpeed: 0, 
+    ySpeed: 0
+};
+
+var paddle = {
+    
+};
+
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    circle.xSpeed = random(-3, 3);
+    circle.ySpeed = random(-3, 3);
+}
+
+function draw() {
+    
+    background(0);
+    ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+    
+    if (circle.x < (0 + circle.diameter/2) || 
+            circle.x > (CANVAS_WIDTH - circle.diameter/2)) {
+        circle.xSpeed = -circle.xSpeed;
+    }
+    
+    if (circle.y < (0 + circle.diameter/2) || 
+            circle.y > (CANVAS_HEIGHT - circle.diameter/2)) {
+        circle.ySpeed = -circle.ySpeed;
+    }
+    
+    
+    circle.x += circle.xSpeed;
+    circle.y += circle.ySpeed;
+
+}
